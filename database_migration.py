6970e5148c662b31339dc13f4e0fc616ae61734a
Josh Cook <joshuacook0023@gmail.com>
2021-03-13 09:34:53 -0500

    style: use proper python script __name__ check

--- database_migration.py
+++ database_migration.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
-from main import coffee_bag_db, coffee_bag_list, convert_bag_to_info
+import main
 
 
 def introduce_active_attribute():
     num_bags = 0
-    for bag in coffee_bag_list():
+    for bag in main.coffee_bag_list():
         num_bags += 1
-        info = convert_bag_to_info(bag)
-        coffee_bag_db.put(info)
+        info = main.convert_bag_to_info(bag)
+        main.coffee_bag_db.put(info)
     print(f"Converted {num_bags} bags.")
 
 
+def migrate():
+    # introduce_active_attribute()
+    print("Done")
+
+
 if __name__ == "__main__":
-    introduce_active_attribute()
+    migrate()
